24b681ecb4cc561f5906321f760731fb2163453b
Surya <surya.elroh@gmail.com>
2017-10-26 15:13:13 +0200

    add camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  app: any = {name: String, version:Number};
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
+    this.app.name = "AppName";
+    this.app.version = 3.0;
+  }
+
+  takePhoto() {
 
   }
+
+  const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.DATA_URL,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
+
+  this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64:
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+  }, (err) => {
+    // Handle error
+  });
 
 }
