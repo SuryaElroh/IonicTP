cf61132ab0e2ce2a7b44f928a1d152f72f3cc1b8
Surya <surya.elroh@gmail.com>
2017-10-26 15:27:57 +0200

    add camera 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,23 +15,21 @@ export class HomePage {
     this.app.version = 3.0;
   }
 
-  takePhoto() {
-
-  }
-
-  const options: CameraOptions = {
+   options: CameraOptions = {
   quality: 100,
   destinationType: this.camera.DestinationType.DATA_URL,
   encodingType: this.camera.EncodingType.JPEG,
   mediaType: this.camera.MediaType.PICTURE
 }
-
-  this.camera.getPicture(options).then((imageData) => {
+takePhoto(){
+  this.camera.getPicture(this.options).then((imageData) => {
     // imageData is either a base64 encoded string or a file URI
     // If it's base64:
-    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    this.img = 'data:image/jpeg;base64,' + imageData;
   }, (err) => {
-    // Handle error
+    console.log(err);
   });
+}
+
 
 }
